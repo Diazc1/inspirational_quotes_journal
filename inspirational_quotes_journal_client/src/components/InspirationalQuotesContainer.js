b1bfed83ad41d5afc4007d6eda6881f0ebc59111
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-14 10:54:52 -0800

    create container to house data and methods revolving around inspirational quotes domain

---
+++ inspirational_quotes_journal_client/src/components/InspirationalQuotesContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class InspirationalQuotesContainer extends Component {
+    render() {
+        return (
+            <div>
+                Inspirational Quotes Container
+            </div>
+        )
+    }
+}
