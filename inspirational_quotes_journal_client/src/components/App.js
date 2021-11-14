686936ac1d356c0ef7643996a34685106bd4adeb
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-14 10:50:45 -0800

    import inspirational quotes container, return in div

---
+++ inspirational_quotes_journal_client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InspirationalQuotesContainer from './InspirationalQuotesContainer';
+
+const App = () => {
+    return (
+        <div>
+            <InspirationalQuotesContainer />
+        </div>
+    );
+};
+
+export default App;
