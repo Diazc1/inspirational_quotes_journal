010ca98c84c1323a408a464c23f411ef27364293
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-14 10:04:27 -0800

    import files rootReducer and App

--- inspirational_quotes_journal_client/src/index.js
+++ inspirational_quotes_journal_client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+import App from './components/App'
+import rootReducer from './reducers/rootReducer'
 
 const store = createStore(
     rootReducer, 
