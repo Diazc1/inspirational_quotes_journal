405ae915a73c2e6b29677107a4d2ee5730c31107
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-15 14:00:12 -0800

    cfetch inspirational quotes case

--- inspirational_quotes_journal_client/src/reducers/inspirationalQuotesReducer.js
+++ inspirational_quotes_journal_client/src/reducers/inspirationalQuotesReducer.js
@@ -1,6 +1,6 @@
 export const inspirationalQuotesReducer = (state = [], action) => {
     switch(action.type){
-        case 'FETCH_GRADS':
+        case 'FETCH_INSPIRATIONAL_QUOTES':
             return action.payload
         default:
             return state
