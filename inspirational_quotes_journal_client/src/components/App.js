1eb6996f9db278dcc62d13f9ceeaf2fdafd95d4b
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-18 15:41:51 -0800

    delete inspirational quote container so it does not show through out my app

--- inspirational_quotes_journal_client/src/components/App.js
+++ inspirational_quotes_journal_client/src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import InspirationalQuotesContainer from './InspirationalQuotesContainer';
 
+import Router from './Router';
+
+//anything you want rendered throughout your app goes in your top level component 
+//navlink goes here
 const App = () => {
     return (
         <div>
-            <InspirationalQuotesContainer />
+            
+            <Router />
         </div>
     );
 };
