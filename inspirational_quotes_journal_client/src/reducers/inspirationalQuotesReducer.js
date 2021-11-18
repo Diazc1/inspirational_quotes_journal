a961898f16039a652950683f0f14406ee6e65c5d
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-18 12:01:42 -0800

    add a case; ADD_INSPIRATIONAL_QUOTE

--- inspirational_quotes_journal_client/src/reducers/inspirationalQuotesReducer.js
+++ inspirational_quotes_journal_client/src/reducers/inspirationalQuotesReducer.js
@@ -2,6 +2,8 @@ export const inspirationalQuotesReducer = (state = [], action) => {
     switch(action.type){
         case 'FETCH_INSPIRATIONAL_QUOTES':
             return action.payload
+        case 'ADD_INSPIRATIONAL_QUOTE':
+            return [...state, action.payload]
         default:
             return state
     }
