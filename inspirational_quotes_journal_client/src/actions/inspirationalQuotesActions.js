30a02a177dc3e2d03f5285469541962b95a45020
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-18 11:59:14 -0800

    create an action that is going to fetch my inspirational quotes from my api, also create an action that will add an inspirational quote

--- inspirational_quotes_journal_client/src/actions/inspirationalQuotesActions.js
+++ inspirational_quotes_journal_client/src/actions/inspirationalQuotesActions.js
@@ -8,3 +8,15 @@ export const fetchInspirationalQuotes = () => {
         .then(inspirational_quotes => dispatch({ type: 'FETCH_INSPIRATIONAL_QUOTES', payload: inspirational_quotes}))
     }
 }
+
+export const addInspirationalQuote = inspirational_quote => {
+    return (dispatch) => {
+        fetch('http://127.0.0.1:3000/inspirational_quotes', {
+            method: 'POST',
+            body: JSON.stringify(inspirational_quote),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(inspirational_quote => dispatch({ type: 'ADD_INSPIRATIONAL_QUOTE', payload: inspirational_quote}))
+    }
+}
