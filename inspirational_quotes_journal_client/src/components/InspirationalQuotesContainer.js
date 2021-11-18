69cf6fbdb3c07ab8d6df9f95a15c1f7511151538
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-18 15:42:54 -0800

    imported inspirational quotes container and mounted

--- inspirational_quotes_journal_client/src/components/InspirationalQuotesContainer.js
+++ inspirational_quotes_journal_client/src/components/InspirationalQuotesContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 //connect my react component to my redux action
 import { connect } from "react-redux";
+//container houses other components
 
 import { fetchInspirationalQuotes } from '../actions/inspirationalQuotesActions';
 //import form and mount 
