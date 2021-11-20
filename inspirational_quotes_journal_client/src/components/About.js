58fa4c7750ba514bf6e3548c6a44d4dfd025dcc5
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-20 09:44:12 -0800

    add image to about page

--- inspirational_quotes_journal_client/src/components/About.js
+++ inspirational_quotes_journal_client/src/components/About.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-
 const About = () => {
     return (
         <div>
-            My About Page
+            {/* <div className="container"> */}
+                <img src="/images/linedpaper.png" alt="" width="1750" height="1200" />
+            {/* </div> */}
         </div>
     );
 };
