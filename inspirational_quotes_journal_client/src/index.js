b6939d4d70de77aaca98b311d436dd053a43c2f9
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-29 23:01:00 -0800

    import app

--- inspirational_quotes_journal_client/src/index.js
+++ inspirational_quotes_journal_client/src/index.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 import { rootReducer } from './reducers/rootReducer'
 
+
 const store = createStore(
     rootReducer, 
     composeWithDevTools(applyMiddleware(thunk))
