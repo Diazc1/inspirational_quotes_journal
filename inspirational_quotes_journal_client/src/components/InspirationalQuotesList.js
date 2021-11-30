fbe5cfd637f8b835a884e4be4cc68970a9ffbdf4
Cathy Diaz <Cathydiaz14@gmail.com>
2021-11-29 23:30:02 -0800

    deleted my li for styling purposes

--- inspirational_quotes_journal_client/src/components/InspirationalQuotesList.js
+++ inspirational_quotes_journal_client/src/components/InspirationalQuotesList.js
@@ -6,16 +6,21 @@ import InspirationalQuotesContainer from './InspirationalQuotesContainer';
 const InspirationalQuotesList = ({ inspirational_quotes }) => {
     return (
         <div>
-            
-            <h1>Inspirational Quotes List:</h1>
-                {inspirational_quotes.map(inspirational_quote => 
-                    <ul key={inspirational_quote.id}>
-                        <li >
-                            {inspirational_quote.quote} - {inspirational_quote.author} - {inspirational_quote.meaning} - {inspirational_quote.date}
-                        </li>
-                    </ul>
-                )}
-        <InspirationalQuotesContainer />
+            <div className="quotes_list">
+                <h1>Inspirational Quotes List:</h1>
+                    {inspirational_quotes.map(inspirational_quote => 
+                            <ul key={inspirational_quote.id}>
+        
+                                    <h5>{inspirational_quote.date}</h5>
+                                    Quote: {inspirational_quote.quote} 
+                                    <br/>
+                                    Author: {inspirational_quote.author}  
+                                    <br/>
+                                    What Does It Mean To You: {inspirational_quote.meaning}
+                            </ul>
+                    )}
+            </div>
+            <InspirationalQuotesContainer />
         </div>
     );
 };
